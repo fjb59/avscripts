e809a4023b912e7e433805f75b70ba246f2fb564
fjb59 <frank@g7wap.me.uk>
2026-02-27 16:48:40 +0000

    Added a movefiles to date stamp folder which reads the datestamp from files and moves said files to folders named after the datestamp
    YYYYMMdd

--- movetodatedfolder.py
+++ movetodatedfolder.py
@@ -1,32 +1,44 @@
+import argparse
 import os.path
 import glob
 from time import ctime
 from pathlib import Path
 import shutil
 from datetime import datetime
-src = "E:\\My Pictures\\wnk\\Hollyoaks\\Pale Brunette"
-if os.path.isdir(src):
-    p = Path(src)
-    files = [f for ext in ("*.jpg", "*.png") for f in p.glob(ext)]
-    for file in files:
 
-        ct = os.path.getctime(file)
-        ft = ctime(ct)
-        #ft = ft.strftime("%a-%b-%d-%Y")
-        dt = datetime.strptime(ft, "%a %b %d %H:%M:%S %Y")
-        date_only = dt.strftime("%a %b %d %Y")
 
-        ft = date_only.replace(" ","-")
-        #ft = ft.replace(":", "_")
-        destFolder = os.path.join(src,ft)
-        baseFile = os.path.basename(file)
-        destFile = os.path.join(destFolder, baseFile)
+def moveFiles(srcFolder):
+    if os.path.isdir(srcFolder):
+        p = Path(srcFolder)
+        files = [f for ext in ("*.jpg", "*.png") for f in p.glob(ext)]
+        for file in files:
 
-        if not os.path.exists(destFolder):
-            print (f"{destFolder} folder does not exist. Creating!")
+            ct = os.path.getctime(file)
+            ft = ctime(ct)
+            #ft = ft.strftime("%a-%b-%d-%Y")
+            dt = datetime.strptime(ft, "%a %b %d %H:%M:%S %Y")
+            #date_only = dt.strftime("%a %b %d %Y")
 
-            os.mkdir(destFolder)
-        print (f"Moving {file} to {destFile}")
-        shutil.move(file,destFile)
-        pass
+            date_only = dt.strftime("%Y %b %d")
+            ft = date_only.replace(" ","-")
+            #ft = ft.replace(":", "_")
+            destFolder = os.path.join(srcFolder,ft)
+            baseFile = os.path.basename(file)
+            destFile = os.path.join(destFolder, baseFile)
 
+            if not os.path.exists(destFolder):
+                print (f"{destFolder} folder does not exist. Creating!")
+
+                os.mkdir(destFolder)
+            print (f"Moving {file} to {destFile}")
+            shutil.move(file,destFile)
+            pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Move files to folder based on their created date")
+    parser.add_argument("source_folder", type=str,
+                        help="Path to text file containing files to be moved.")
+    args = parser.parse_args()
+    srcFolder = args.source_folder
+
+    moveFiles(srcFolder=srcFolder)
