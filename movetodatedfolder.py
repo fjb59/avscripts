2787d5808fef5b1cb7b5c2641f7c14bccfa2a78d
fjb59 <frank@g7wap.me.uk>
2026-02-28 13:24:32 +0000

    Added a movefiles to date stamp folder which reads the datestamp from files and moves said files to folders named after the datestamp
    YYYYMMdd

--- movetodatedfolder.py
+++ movetodatedfolder.py
@@ -6,8 +6,12 @@ from time import ctime
 from pathlib import Path
 import shutil
 from datetime import datetime
+from PIL import Image
+from PIL.ExifTags import TAGS
 
-def getfileDateFromFile(tFilename,pattern = r'\D(\d{8}|\d{4}-\d{2}-\d{2})\D'):
+
+
+def getfileDateFromFile(tFilename,pattern = r'\D(\d{8}|\d{4}-\d{2}-\d{2})\D', verbose =False):
 #r'[_-](\d{8})[_-]'
     if os.path.exists(tFilename):
         if pattern == "-":
@@ -18,6 +22,8 @@ def getfileDateFromFile(tFilename,pattern = r'\D(\d{8}|\d{4}-\d{2}-\d{2})\D'):
         else:
 
             filename = os.path.basename(tFilename)
+
+#try getting date from filename via regex pattern
 
             match = re.search(pattern, filename)
             if match is not None:
@@ -34,10 +40,31 @@ def getfileDateFromFile(tFilename,pattern = r'\D(\d{8}|\d{4}-\d{2}-\d{2})\D'):
 
 
             else:
-                ct = os.path.getctime(tFilename)
-                date_str= ctime(ct)
-                dt = datetime.strptime(date_str, "%a %b %d %H:%M:%S %Y")
-                year_only = os.path.join("predated",dt.strftime("%Y"))
+#pattern match failed. Try reading metadata.
+                img = Image.open(tFilename)
+                exif =img.getexif()
+                if exif:
+                    date_taken = None
+                    for tag_id, value in exif.items():
+                        tag = TAGS.get(tag_id, tag_id)
+                        if TAGS.get(tag_id) == "DateTime":
+                            dt = datetime.strptime(value, "%Y:%m:%d %H:%M:%S")
+
+                            year_only = dt.strftime("%Y")
+                            date_only = dt.strftime("%m%d")
+                            return  year_only,date_only
+
+                        else:
+                            if verbose:
+                                print(f"{tag}: {value}")
+                else:
+
+
+
+                        ct = os.path.getctime(tFilename)
+                        date_str= ctime(ct)
+                        dt = datetime.strptime(date_str, "%a %b %d %H:%M:%S %Y")
+                        year_only = os.path.join("predated",dt.strftime("%Y"))
 
 
 
@@ -45,42 +72,39 @@ def getfileDateFromFile(tFilename,pattern = r'\D(\d{8}|\d{4}-\d{2}-\d{2})\D'):
         date_only = dt.strftime("%b %d")
         ft = date_only.replace(" ", "-")
         return year_only,ft
-def moveFiles(srcFolder):
+def moveFiles(srcFolder,verbose = False):
     if os.path.isdir(srcFolder):
         p = Path(srcFolder)
         files = [f for ext in ("*.jpg", "*.png") for f in p.glob(ext)]
         for file in files:
 
-            # ct = os.path.getctime(file)
-            # ft = ctime(ct)
-            # #ft = ft.strftime("%a-%b-%d-%Y")
-            # dt = datetime.strptime(ft, "%a %b %d %H:%M:%S %Y")
-            # #date_only = dt.strftime("%a %b %d %Y")
-            #
-            # date_only = dt.strftime("%Y %b %d")
-            # ft = date_only.replace(" ","-")
-            # #ft = ft.replace(":", "_")
-            year,ft = getfileDateFromFile(file)
+            year,ft = getfileDateFromFile(file,verbose=verbose)
             yearFolder = os.path.join(srcFolder, year)
             destFolder = os.path.join(yearFolder,ft)
             baseFile = os.path.basename(file)
             destFile = os.path.join(destFolder, baseFile)
 
             if not os.path.exists(destFolder):
-                print (f"{destFolder} folder does not exist. Creating!")
+                if verbose:
+                    print (f"{destFolder} folder does not exist. Creating!")
                 os.makedirs(destFolder,exist_ok=True)
                 #os.mkdir(destFolder)
-            print (f"Moving {file} to {destFile}")
+            if verbose:
+                print (f"Moving {file} to {destFile}")
             shutil.move(file,destFile)
-            pass
 
+    else:
+        print (f"{srcFolder} is not a folder.")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Move files to folder based on their created date")
     parser.add_argument("source_folder", type=str,
                         help="Path to text file containing files to be moved.")
+    parser.add_argument("--verbose", type=str, default="false",
+                        help="display activity.")
     args = parser.parse_args()
     srcFolder = args.source_folder
+    verbose = args.verbose.lower() =="true"
 
     moveFiles(srcFolder=srcFolder)
 
-    #datestr = getfileDateFromFile("E:\My Pictures\soaps\Corrie\Alison King\Alison-20241230_20002100_11671.jpg")
+    #datestr = getfileDateFromFile(r"E:\My Pictures\wnk\Corrie\Alison King\Alison-Coronation Street_11176.jpg")
